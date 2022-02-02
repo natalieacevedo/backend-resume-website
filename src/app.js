b0ba394e270a5eb0fa6aa4531e4a5bcde93cae25
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-02-02 19:08:29 +0000

    finished backend configuration

---
+++ src/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+//const trackRoutes = require('./routes/trackRoutes');
+
+const app = express();
+
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const app = require('./app');
+//import the database connection
+const connection = require('./db-config');
+//connect database
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+  } else {
+    console.log(
+      'connected to database with threadId :  ' + connection.threadId
+    );
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, (err) => {
+  // eslint-disable-next-line no-console
+  if (err) console.error(err);
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
