8ff8b19d11e7c82668215b8d4516f9049bae804c
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-02-03 11:54:39 +0000

    form works

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-
 const projectsRoutes = require('./routes/projectsRoutes');
 const imagesRoutes = require('./routes/imagesRoutes');
 const contactRoutes = require('./routes/contactRoutes');
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+
+//Geomar change this according to your react port
+let headerCors = 3002
+
+app.use(
+  cors({
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "cookies"],
+    origin: [`http://localhost:${headerCors}`],
+  })
+);
 
 app.use('/api/projects', projectsRoutes);
 //app.use('api/pictures', imagesRoutes);
